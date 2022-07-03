3ab1d4d5ad088be23194564478627d72db601063
PabloMultifunciones <pablo_pages@live.com>
2022-07-03 14:35:52 -0300

    fix: i had added middleware customized so loading of the components is faster

--- src/store/configStore.ts
+++ src/store/configStore.ts
@@ -1,6 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+/* eslint-disable no-unused-vars */
+import { configureStore, PreloadedState } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
-const store = configureStore({ reducer: reducers });
+// eslint-disable-next-line no-use-before-define
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+    return configureStore({
+        reducer: reducers,
+        preloadedState,
+        middleware: [thunk],
+    });
+}
 
-export default store;
+export type RootState = ReturnType<typeof reducers>;
+export type AppStore = ReturnType<typeof setupStore>;
