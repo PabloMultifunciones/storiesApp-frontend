2bc37fd0d1c0a3555f3cc8f91e79378c197e33d2
PabloMultifunciones <pablo_pages@live.com>
2022-07-03 15:29:07 -0300

    fix: i have fixed the import route of the setupStore

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -4,9 +4,9 @@ import type { RenderOptions } from '@testing-library/react';
 import { MemoryRouter as Router } from 'react-router-dom';
 import type { PreloadedState } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import { setupStore } from '../../store/configStore';
+import { setupStore } from '../store/configStore';
 
-import type { AppStore, RootState } from '../../store/configStore';
+import type { AppStore, RootState } from '../store/configStore';
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
     preloadedState?: PreloadedState<RootState>;
